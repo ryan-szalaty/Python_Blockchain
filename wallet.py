4af04142a3f429cf68fd9e4e04cc1eaaeb359f86
Ryan <ryan.szalaty@gmail.com>
2021-06-21 15:55:24 +0900

    Created public and private keys.

--- wallet.py
+++ wallet.py
@@ -1,3 +1,5 @@
+"""All imports from pycryptodome"""
+
 from Crypto.PublicKey import RSA
 import Crypto.Random
 import binascii
@@ -5,9 +7,38 @@ import binascii
 
 class Wallet:
     def __init__(self):
+        self.private_key = None
+        self.public_key = None
+
+
+    def create_keys(self):
         private_key, public_key = self.generate_keys()
         self.private_key = private_key
         self.public_key = public_key
+        
+
+    def save_keys(self):
+        if self.public_key != None and self.private_key != None:
+            try:
+                with open('wallet.txt', 'w') as file:
+                    file.write(self.public_key)
+                    file.write('\n')
+                    file.write(self.private_key)
+            except (IOError, IndexError):
+                print('Saving wallet failed...')
+
+    def load_keys(self):
+        try:
+            with open('wallet.txt', 'r') as file:
+                keys = file.readlines()
+                public_key = keys[0][:-1]
+                private_key = keys[1]
+                self.public_key = public_key
+                self.private_key = private_key
+        except (IOError, IndexError):
+            print('Loading wallet failed...')
+
+
 
     def generate_keys(self):
         private_key = RSA.generate(1024, Crypto.Random.new().read)
